390fe528e78abe6786656a9a805a1e5ce3abc57e
joemacko <joemacko58@gmail.com>
2021-01-12 20:33:13 -0500

    worked through Program, Pokemon, and Pokemon_Repository files correcting build errors. Had some left to go in ProgramUI, but made it through most

---
+++ Pokemon_Repository/Pokemon_Repository.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pokemon_Repository
+{
+    public class PokemonRepository
+    {
+        private List<Pokemon> _pokemonTeam = new List<Pokemon>();
+
+        //add to list (limit pokemon to 6)
+        public void AddPokemonToTeam(Pokemon pokemon)
+        {
+            _pokemonTeam.Add(pokemon);
+        }
+        //get list
+        public List<Pokemon> GetPokemonTeam()
+        {
+            return _pokemonTeam;
+        }
+        //get one pokemon
+        public Pokemon GetPokemonByTeamPosition(int teamPosition)
+        {
+            Pokemon pokemon = _pokemonTeam[teamPosition - 1];
+            return pokemon;
+        }
+        //update pokemon
+        public void UpdatePokemonByTeamPosition(int teamPosition, Pokemon updates)
+        {
+            Pokemon pokemon = _pokemonTeam[teamPosition - 1];
+            pokemon.PokemonSpeciesName = updates.PokemonSpeciesName;
+            pokemon.PokemonNickName = updates.PokemonNickName;
+            pokemon.Level = updates.Level;
+            pokemon.PokemonType = updates.PokemonType;
+            pokemon.SecondaryType = updates.SecondaryType;
+            pokemon.MoveOne = updates.MoveOne;
+            pokemon.MoveTwo = updates.MoveTwo;
+            pokemon.MoveThree = updates.MoveThree;
+            pokemon.MoveFour = updates.MoveFour;
+        }
+
+        public void UpdatePokemonByNickName(string nickName, Pokemon newPokemon)
+        {
+            foreach (Pokemon p in _pokemonTeam)
+            {
+                if (nickName == p.PokemonNickName)
+                {
+                    p.PokemonSpeciesName = newPokemon.PokemonSpeciesName;
+                    p.PokemonNickName = newPokemon.PokemonNickName;
+                    p.Level = newPokemon.Level;
+                    p.PokemonType = newPokemon.PokemonType;
+                    p.SecondaryType = newPokemon.SecondaryType;
+                    p.MoveOne = newPokemon.MoveOne;
+                    p.MoveTwo = newPokemon.MoveTwo;
+                    p.MoveThree = newPokemon.MoveThree;
+                    p.MoveFour = newPokemon.MoveFour;
+                }
+            }
+        }
+
+        //remove pokemon
+        public void RemovePokemonFromTeam(int teamPosition)
+        {
+            Pokemon pokemon = _pokemonTeam[teamPosition - 1];
+            _pokemonTeam.Remove(pokemon);
+        }
+    }
+}
